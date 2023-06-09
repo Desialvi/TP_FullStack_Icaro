0d2aa8280d9587e6bb72122c7d4c3403fcd5cbfc
Desialvi <desialvi@gmail.com>
2023-06-09 15:32:36 -0300

    Mi primer TP

---
+++ TP1.js
@@ -0,0 +1,8 @@
+
+//4. Inicia dos variables numéricas y asignales cualquier valor numérico
+let num1 = 10;
+let num2 = 20;
+//5. Suma las variables creadas en el punto anterior y guarda el resultado en una tercara variable llamada resultado
+let resultado = num1 + num2
+// Muestra la variable resultado en consula
+console.log(resultado)
