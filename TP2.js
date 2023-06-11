d7c2e73e6e29da12d95c5ac71fb0de77fa320cc0
Desialvi <desialvi@gmail.com>
2023-06-11 12:24:24 -0300

    Pregunta 2 del TP2

---
+++ TP2.js
@@ -0,0 +1,32 @@
+// La variable definida abajo almacena un numero entero entre 1 y 100 de forma aleatoria, 
+//es decir que cada vez que se vuelva a ejectuar el codigo obtendrá un número nuevo aleatorio.
+
+const numeroRandom = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+
+//Usar esta variable para determinar si el número es par o impar (Un número es par si el resto de 
+//la división sobre 2 es igual a 0). Para calcular el resto usar: resto = dividendo % 2.
+
+function paroimpar(numero) {
+    if (numero %2 == 0 ) {
+        return "El número " + numeroRandom + " " + "es Par"
+    }
+    return "El número " + numeroRandom + " " + "no es Par"
+}
+
+console.log(paroimpar(numeroRandom))
+
+//Dada dos palabras, "tren" y "edificio", hacer un programa que nos permita calcular cuál palabra es más larga. Usar la función length
+
+function longPalabra(Palabra1, Palabra2) {
+    LongP1= Palabra1.length
+    LongP2= Palabra2.length
+    if (LongP1 > LongP2 ) {
+        return "La palabra " + Palabra1 + " " + "es mas larga que " + Palabra2
+    }
+    else if (LongP1 == LongP2) {
+        return "La palabra " + Palabra1 + " " + "es igual de larga que la " + Palabra2 
+    }
+    return "La palabra " + Palabra2 + " " + "es mas larga que " + Palabra1
+} 
+
+console.log(longPalabra("tren", "edificio"))
