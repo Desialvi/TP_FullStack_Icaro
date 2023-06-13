3fefe1fdc19ec77a772069eff3c40b3e68da90fa
Desialvi <desialvi@gmail.com>
2023-06-13 20:45:47 -0300

    TP3 ejercicio 2

--- TP3.js
+++ TP3.js
@@ -9,3 +9,13 @@ while (i <= 10) {
    console.log( userWord)
    i=i+1
 }
+
+//2.Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+const userEdad = readlineSync.question("Ingrese su edad: ")
+let j=1
+let año= 2024 - userEdad
+while (j <= userEdad) {
+   console.log("Edad en el año= ",año, "es de ", j)
+   j=j+1
+   año++ 
+}
